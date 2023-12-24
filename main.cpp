4117328fdc59308c216e335197fd952a4203c8a0
denisovamaria <mvdenisova.work@gmail.com>
2023-12-24 15:18:16 +0300

    done

--- main.cpp
+++ main.cpp
@@ -4,13 +4,19 @@
 
 using namespace std;
 
-pair<int, string> longestPalindrome(const string& s) {
+int main() {
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+
+    string s;
+    input >> s;
+
     int n = s.length();
 
-    vector<vector<int>> dp(n, vector<int>(n, 0));
+    vector<vector<int>> matrix(n, vector<int>(n, 0));
 
     for (int i = 0; i < n; ++i) {
-        dp[i][i] = 1;
+        matrix[i][i] = 1;
     }
 
     for (int len = 2; len <= n; ++len) {
@@ -18,16 +24,16 @@ pair<int, string> longestPalindrome(const string& s) {
             int j = i + len - 1;
 
             if (s[i] == s[j] && len == 2) {
-                dp[i][j] = 2;
+                matrix[i][j] = 2;
             } else if (s[i] == s[j]) {
-                dp[i][j] = dp[i + 1][j - 1] + 2;
+                matrix[i][j] = matrix[i + 1][j - 1] + 2;
             } else {
-                dp[i][j] = max(dp[i][j - 1], dp[i + 1][j]);
+                matrix[i][j] = max(matrix[i][j - 1], matrix[i + 1][j]);
             }
         }
     }
 
-    int length = dp[0][n - 1];
+    int length = matrix[0][n - 1];
 
     string palindrome;
     palindrome.reserve(length);
@@ -38,7 +44,7 @@ pair<int, string> longestPalindrome(const string& s) {
             palindrome.push_back(s[i]);
             ++i;
             --j;
-        } else if (dp[i][j - 1] > dp[i + 1][j]) {
+        } else if (matrix[i][j - 1] > matrix[i + 1][j]) {
             --j;
         } else {
             ++i;
@@ -53,20 +59,8 @@ pair<int, string> longestPalindrome(const string& s) {
         palindrome.push_back(palindrome[k]);
     }
 
-    return make_pair(length, palindrome);
-}
-
-int main() {
-    ifstream input("input.txt");
-    ofstream output("output.txt");
-
-    string s;
-    input >> s;
-
-    pair<int, string> result = longestPalindrome(s);
-
-    output << result.first << endl;
-    output << result.second << endl;
+    output << length << endl;
+    output << palindrome << endl;
 
     input.close();
     output.close();
